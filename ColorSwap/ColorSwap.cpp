1e5bcf60b8e9dc43f2791b7cebe4f2c7181f149d
Patryk Gamrat <Patryk.gam@gmail.com>
2023-05-31 19:00:51 +0200

    Add precompiled header

---
+++ ColorSwap/ColorSwap.cpp
@@ -0,0 +1,1 @@
+#include "ColorSwap.h"
---
+++ ColorSwap/ColorSwap.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+//Constants
