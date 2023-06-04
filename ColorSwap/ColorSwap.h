d7edbcf0140ebf898a6930d19a327b6821d716f9
karozaj <karol08@vp.pl>
2023-06-04 16:53:37 +0200

    color constants

--- ColorSwap/ColorSwap.h
+++ ColorSwap/ColorSwap.h
@@ -16,3 +16,8 @@ const int WINDOW_HEIGHT = 800; //px;
 
 const float GRAVITY = 0.5f;
 const float JUMP_HEIGHT = -5.f;
+
+const sf::Color COLOR_SWAP_CYAN(31, 77, 82);
+const sf::Color COLOR_SWAP_PURPLE(49, 7, 82);
+const sf::Color COLOR_SWAP_YELLOW(87, 76, 20);
+const sf::Color COLOR_SWAP_RED(94, 5, 27);
