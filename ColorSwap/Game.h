d95f9125740c849e916f0003b5c5dca4d582b65b
Patryk Gamrat <Patryk.gam@gmail.com>
2023-06-08 21:11:56 +0200

    Improved error window

--- ColorSwap/Game.h
+++ ColorSwap/Game.h
@@ -7,7 +7,9 @@
 
 using namespace sf;
 
-enum GameState {Menu, Play, GameOver, Error};
+//Possible game states
+enum GameState { Menu, Play, GameOver, Error };
+//Integer distribution for random obstacle generation
 static std::uniform_int_distribution<int> obstacleRandomPick(0, 0);
 
 class Game
