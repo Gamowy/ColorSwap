45585f0c51e51f1e05aeeb7547be65c8531a0f26
karozaj <karol08@vp.pl>
2023-06-09 19:38:37 +0200

    added double ring obstacle

--- ColorSwap/ObstacleRingOfRectangles.h
+++ ColorSwap/ObstacleRingOfRectangles.h
@@ -1,11 +1,15 @@
 #pragma once
 #include"ColorSwap.h"
 #include "Obstacle.h"
+//Integer distribution for ring variation
+static std::uniform_int_distribution<int> obstacleRingVariation(0, 1);
+
 class ObstacleRingOfRectangles : virtual public Obstacle
 {
 private:
+    int ringVariation;
     Vector2f center;
-    RectangleShape rectangles[12];
+    RectangleShape* rectangles;
     float rotationSpeed;
 public:
     ObstacleRingOfRectangles(float yPosition, Texture& starTexture, Texture& colorSwitchTexture);
